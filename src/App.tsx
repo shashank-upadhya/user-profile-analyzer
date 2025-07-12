2d96ed6fd38ec09eac95a6621eb185fa62e0aca7
Shashank Upadhya <shashankupadhya813@gmail.com>
2025-07-12 18:12:14 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import UserProfile from './components/UserProfile';
-import CommitChart from './components/CommitChart';
 
 const App: React.FC = () => {
-  const [githubUsername, setGithubUsername] = useState<string | null>(null);
-
-  const handleUsernameSubmit = useCallback((username: string) => {
-    setGithubUsername(username);
-  }, []);
-
   return (
-    <div>
-      <UserProfile onUsernameSubmit={handleUsernameSubmit} />
-      {githubUsername && <CommitChart username={githubUsername} />}
+    <div className="min-h-screen bg-gray-50">
+      <UserProfile />
     </div>
   );
 };
